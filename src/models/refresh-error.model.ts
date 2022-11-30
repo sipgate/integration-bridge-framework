cdb3319b766dbc3146d88760246af8c31c0a7084
Leona Kuse <kuse@sipgate.de>
2022-11-30 15:22:49 +0100

    :necktie: adapt refresh error code

--- src/models/refresh-error.model.ts
+++ src/models/refresh-error.model.ts
@@ -1,8 +1,8 @@
 export class RefreshError extends Error {
   public status: number;
 
-  constructor(message?: string) {
-    super(`Token Refresh Error${message ? `: ${message}` : ""}`);
-    this.status = 407;
+  constructor() {
+    super(`Invalid integration refresh token`);
+    this.status = 400;
   }
 }
