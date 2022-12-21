a93350f085cd4c62d90c56b1bc2e3e13b64f8567
Leona Kuse <kuse@sipgate.de>
2022-12-21 14:54:30 +0100

    0.12.13

--- src/util/anonymize-key.test.ts
+++ src/util/anonymize-key.test.ts
@@ -2,12 +2,12 @@ import { anonymizeKey } from "./anonymize-key";
 
 it("anonymizes valid key correctly", () => {
   const originalKey = "1n2n3k23j43j23j4bdnc";
-  const anonymizedKey = "...3j23j4bdnc";
+  const anonymizedKey = "***3j23j4bdnc";
   expect(anonymizeKey(originalKey)).toEqual(anonymizedKey);
 });
 
 it("anonymizes empty key correctly", () => {
   const originalKey = "";
-  const anonymizedKey = "...";
+  const anonymizedKey = "***";
   expect(anonymizeKey(originalKey)).toEqual(anonymizedKey);
 });
