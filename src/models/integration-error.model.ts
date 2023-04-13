7d69ccdee63fe2298126988f5d88c3a941a011b4
Leona Kuse <kuse@sipgate.de>
2023-04-13 10:10:16 +0200

    :goal_net: add generic frontend error code

--- src/models/integration-error.model.ts
+++ src/models/integration-error.model.ts
@@ -1,3 +1,6 @@
 export enum IntegrationErrorType {
   INTEGRATION_REFRESH_ERROR = "integration/refresh-error",
+  CONTACT_CREATE_ERROR_CONFLICT = "contact/create-error/conflict",
 }
+
+export const DELEGATE_TO_FRONTEND_CODE = 452;
