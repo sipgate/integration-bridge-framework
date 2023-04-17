1f041b3c5b2beb3bdee01f359b5dfb69b55cf902
Leona Kuse <kuse@sipgate.de>
2023-04-17 08:35:43 +0200

    :goal_net: add more bridge to frontend error types

--- src/models/integration-error.model.ts
+++ src/models/integration-error.model.ts
@@ -1,6 +1,7 @@
 export enum IntegrationErrorType {
   INTEGRATION_REFRESH_ERROR = "integration/refresh-error",
   CONTACT_CREATE_ERROR_CONFLICT = "contact/create-error/conflict",
+  CONTACT_CREATE_ERROR_EMAIL_CONFLICT = "contact/create-error/email-conflict",
 }
 
 export const DELEGATE_TO_FRONTEND_CODE = 452;
