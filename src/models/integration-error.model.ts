8f88276e3df4e55b22cbd83c4beb022c06c21b83
Leon Erath <erath@sipgate.de>
2023-05-03 10:52:06 +0200

    :goal_net: Add too many numbers error

--- src/models/integration-error.model.ts
+++ src/models/integration-error.model.ts
@@ -3,6 +3,8 @@ export enum IntegrationErrorType {
   INTEGRATION_REFRESH_ERROR = "integration/refresh-error",
   CONTACT_CREATE_ERROR_CONFLICT = "contact/create-error/conflict",
   CONTACT_CREATE_ERROR_EMAIL_CONFLICT = "contact/create-error/email-conflict",
+  CONTACT_CREATE_ERROR_TOO_MANY_NUMBERS = "contact/create-error/too-many-numbers",
+  CONTACT_UPDATE_ERROR_TOO_MANY_NUMBERS = "contact/update-error/too-many-numbers",
 }
 
 export const DELEGATE_TO_FRONTEND_CODE = 452;
