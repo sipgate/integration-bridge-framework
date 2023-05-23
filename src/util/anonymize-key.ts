39f537c3d055c59577b6e07b97e5dde43a7de5c6
Leona Kuse <kuse@sipgate.de>
2023-05-23 12:59:43 +0200

    :wrench: fix bug in logger function

--- src/util/anonymize-key.ts
+++ src/util/anonymize-key.ts
@@ -1,5 +1,9 @@
 const ANONYMIZED_KEY_LENGTH: number = 10;
 
-export function anonymizeKey(key: string): string {
+export function anonymizeKey(key: unknown): string {
+  if (!key || typeof key !== "string") {
+    return "UNKNOWN";
+  }
+
   return `***${key.slice(key.length - ANONYMIZED_KEY_LENGTH)}`;
 }
