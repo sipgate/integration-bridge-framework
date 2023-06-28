3d7aba816df647a1261ba908c2cf5d873cb0dd66
Sebastian Ratkowski <ratkowski@sipgate.de>
2023-06-28 09:34:43 +0200

    :sparkles: Add typeguard for integration entities

---
+++ src/util/integration-entity.ts
@@ -0,0 +1,10 @@
+import { IntegrationEntity, LoggedIntegrationEntity } from "../models";
+
+export const isLoggedIntegrationEntity = (
+  e: IntegrationEntity | LoggedIntegrationEntity
+): e is LoggedIntegrationEntity => {
+  return "logId" in e && typeof e.logId === "string" && e.logId !== "";
+};
+export const isIntegrationEntity = (
+  e: IntegrationEntity | LoggedIntegrationEntity
+): e is IntegrationEntity => !isLoggedIntegrationEntity(e);
