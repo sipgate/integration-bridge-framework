c3f1ed277e5387880bd76b821a71f53426b27e75
Sam Vogelskamp <vogelskamp@sipgate.de>
2023-07-04 14:43:25 +0200

    :technologist: add race condition to catch missing application-default reauth

--- src/models/pubsub-client.model.ts
+++ src/models/pubsub-client.model.ts
@@ -1,6 +1,8 @@
 import { PubSub } from "@google-cloud/pubsub";
 import { PubSubContactsMessage } from "./pubsub-contacts-message.model";
+import { ServerError } from "./server-error.model";
 
+const PUBLISH_TIMEOUT = 10_000;
 export class PubSubClient {
   private client: PubSub;
   private topicName: string;
@@ -15,9 +17,27 @@ export class PubSubClient {
       throw new Error("No pubsub topic name provided.");
     }
 
+    const timeout = (ms: number) =>
+      new Promise((_, reject) =>
+        setTimeout(
+          () =>
+            reject(
+              new ServerError(
+                500,
+                "Could not publish message in time, did you forget to run gcloud auth application-default login?"
+              )
+            ),
+          ms
+        )
+      );
+
     const json = JSON.stringify(message);
     const dataBuffer = Buffer.from(json);
     const topic = this.client.topic(this.topicName);
-    await topic.publishMessage({ data: dataBuffer });
+
+    await Promise.race([
+      topic.publishMessage({ data: dataBuffer }),
+      timeout(PUBLISH_TIMEOUT),
+    ]);
   }
 }
