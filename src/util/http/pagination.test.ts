aa09897fc2fe6757040a861bdc797781a593349f
Oliver Wegner <void1976@gmail.com>
2023-07-19 15:10:13 +0200

    [pagination] implement test

--- src/util/http/pagination.test.ts
+++ src/util/http/pagination.test.ts
@@ -1,9 +1,42 @@
+//import type { AxiosResponse } from 'axios';
 import { paginate } from './pagination';
 
-describe('pagination', () => {
-  it('should paginate over all pages', () => {
-    const i = 0;
+function* fetchDataGen(chunkSize: number, items: number): any {
+  let itemsLeft = items;
 
-    expect(i).toBeLessThan(5);
+  while (itemsLeft > 0) {
+    const n = Math.max(0, Math.min(chunkSize, itemsLeft));
+
+    yield new Array(n).fill(0).map((x) => ({
+      name: `${Math.random()}`,
+      value: Math.random(),
+    }));
+
+    itemsLeft -= n;
+  }
+}
+
+describe('pagination', () => {
+  it('should paginate over all pages', async () => {
+    const fetchDataIterator = fetchDataGen(3, 8);
+    const fetchData = () =>
+      Promise.resolve({
+        data: fetchDataIterator.next().value,
+        status: 200,
+        statusText: 'OK',
+        headers: {},
+        config: {},
+      });
+
+    const data = await paginate<Array<any>>(
+      (data, newData) => [...(data || []), ...(newData || [])],
+      (response) => response?.data,
+      (response) => (response?.data?.length || 0) < 3,
+      (previousResponse, data) => fetchData(),
+      0,
+      [],
+    );
+
+    expect(data).toHaveLength(8);
   });
 });
