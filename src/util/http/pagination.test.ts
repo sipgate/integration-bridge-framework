9c3f1101a3b85af732de3bf4107053f29bd251f3
Oliver Wegner <void1976@gmail.com>
2023-07-19 13:36:56 +0200

    [pagination] add test file

---
+++ src/util/http/pagination.test.ts
@@ -0,0 +1,9 @@
+import { paginate } from './pagination';
+
+describe('pagination', () => {
+  it('should paginate over all pages', () => {
+    const i = 0;
+
+    expect(i).toBeLessThan(5);
+  });
+});
