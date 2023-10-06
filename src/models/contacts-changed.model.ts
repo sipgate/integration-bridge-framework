b6d3a6cf1ffc327c8fe7da81e33b97734896a6d0
Malte Sehmer <sehmer@sipgate.de>
2023-10-06 14:44:19 +0200

    :building_construction: Change ContactsChangedData model

--- src/models/contacts-changed.model.ts
+++ src/models/contacts-changed.model.ts
@@ -1,11 +1,8 @@
-import { IntegrationEntityType } from './integration-entity.model';
-
 export type ContactsChangedData = {
   integrationName: string;
   data: {
     integrationAccountId: string;
     type: 'UPDATE' | 'CREATE' | 'DELETE';
-    objectId: string;
-    integrationEntityType: IntegrationEntityType;
+    contactId: string;
   }[];
 };
