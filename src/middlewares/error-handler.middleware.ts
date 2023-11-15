699677e780b8ec33834b60c81966d13541dfeb62
Leona Kuse <kuse@sipgate.de>
2023-11-15 16:36:04 +0100

    :wrench: re-add next parameter for middleware

--- src/middlewares/error-handler.middleware.ts
+++ src/middlewares/error-handler.middleware.ts
@@ -1,10 +1,12 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { ServerError } from '../models';
 
 export function errorHandlerMiddleware(
   error: Error | ServerError,
   req: Request,
   res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  next: NextFunction,
 ): void {
   if (error instanceof ServerError) {
     res.status(error.status).send(error.message);
