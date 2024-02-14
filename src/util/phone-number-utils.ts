463aabbb6cdd0dd066f86c5270bc0b04e58b4f24
Leona Kuse <kuse@sipgate.de>
2024-02-14 17:13:58 +0100

    :loud_sound: add log message for parsing number error

--- src/util/phone-number-utils.ts
+++ src/util/phone-number-utils.ts
@@ -1,6 +1,6 @@
 import { parsePhoneNumber as parse } from 'awesome-phonenumber';
-import { PhoneNumber, PhoneNumberType } from '../models';
-import { APIPhoneNumber } from '../models/api-contact.model';
+import { APIPhoneNumber, PhoneNumber, PhoneNumberType } from '../models';
+import { errorLogger } from './logger.util';
 
 const MIN_PHONE_NUMBER_LENGTH = 5;
 
@@ -18,23 +18,32 @@ export const parsePhoneNumber = (
     : PhoneNumberType.STANDARD;
   try {
     const region = locale.replace('_', '-').replace(/.+-/, '').toUpperCase();
-    const parsedPhoneNumber = parse(phoneNumber, region);
-    const nationalNumber = parsedPhoneNumber.getNumber('national');
+    const parsedPhoneNumber = parse(phoneNumber, {
+      regionCode: region,
+    });
+    const nationalNumber = parsedPhoneNumber.number?.national;
 
-    const phoneNumberRegion = parsedPhoneNumber.getRegionCode();
-    const e164 = parsedPhoneNumber.getNumber('e164') ?? phoneNumber;
-    const newVar = {
+    const phoneNumberRegion = parsedPhoneNumber.regionCode;
+    const e164 = parsedPhoneNumber.number?.e164 ?? phoneNumber;
+
+    return {
       label,
       type,
       e164: isNumberDirectDial ? phoneNumber : e164,
       localized:
         ignoreRegion || region === phoneNumberRegion
           ? nationalNumber ?? phoneNumber
-          : parsedPhoneNumber.getNumber('international') ?? phoneNumber,
+          : parsedPhoneNumber.number?.international ?? phoneNumber,
       phoneNumber: e164,
     };
-    return newVar;
   } catch {
+    errorLogger(
+      'parsePhoneNumber',
+      'could not split into localized and e164, returning plain phonenumber',
+      '...',
+      { phoneNumber },
+    );
+
     return {
       label,
       type,
