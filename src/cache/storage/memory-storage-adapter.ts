b721c2b8a1bcecc59612907b1599d0028593676c
Leona Kuse <kuse@sipgate.de>
2024-07-29 08:56:33 +0200

    🏷️ fix getting elem from cache after new dependency version

--- src/cache/storage/memory-storage-adapter.ts
+++ src/cache/storage/memory-storage-adapter.ts
@@ -21,8 +21,11 @@ export class MemoryStorageAdapter implements StorageAdapter {
   }
 
   public async get<T>(key: string): Promise<T | null> {
-    const cached = await this.cache.get<T>(key);
-    return cached ?? null;
+    if (this.cache.has(key)) {
+      return this.cache.get(key) as T;
+    }
+
+    return null;
   }
 
   public async set<T>(key: string, value: T): Promise<void> {
